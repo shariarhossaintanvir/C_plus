6088bb31898ea35eb21913153065da04b0acf5f5
shariarhossaintanvir <bangboombro629@gmail.com>
2025-08-19 01:16:43 +0600

    Add Initial Files

---
+++ 011_prime_1-N.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N;
+    cout << "Enter the value of N: ";
+    cin >> N;
+    cout << "Prime numbers from 1 to " << N << " are:\n";
+    for (int i = 2; i <= N; i++) {
+        bool isPrime = true;
+        for (int j = 2; j < i; j++) {
+            if (i % j == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            cout << i << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
