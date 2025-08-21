5652497fa82fe4ec342ce246f8e21bdfba34b5be
shariarhossaintanvir <bangboombro629@gmail.com>
2025-08-22 01:15:49 +0600

    Add Initial Files

---
+++ 017_even_or_odd.cpp
@@ -0,0 +1,29 @@
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int num;
+//     cout << "Enter an integer: ";
+//     cin >> num;
+
+//     if(num % 2 == 0)
+//         cout << num << " is Even." << endl;
+//     else
+//         cout << num << " is Odd." << endl;
+
+//     return 0;
+// }
+
+#include <iostream>
+using namespace std;
+int number ;
+int main (){
+    cout<<"Enter a number : ";
+    cin>>number;
+    if (number % 2 == 0)
+    cout<<number <<"is even." <<endl;
+    cout<<number <<"is odd." << endl;
+    return 0;
+
+
+}
