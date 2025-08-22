68344eb2afb38e9c268370da4c52fa1eccead19a
shariarhossaintanvir <bangboombro629@gmail.com>
2025-08-22 16:58:44 +0600

    Add Initia Files

---
+++ 021_A_to_Z.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char ch; 
+    cout << "Alphabets from A to Z:" << endl;
+    for(ch = 'A'; ch <= 'Z'; ch++) {
+    cout << ch << " ";
+    }
+    return 0;
+}
