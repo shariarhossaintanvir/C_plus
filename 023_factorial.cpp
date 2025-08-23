162797988eb63968d4f76eaba27901573128eff6
shariarhossaintanvir <bangboombro629@gmail.com>
2025-08-24 00:35:22 +0600

    Add Initial Files

---
+++ 023_factorial.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n;
+    unsigned long long factorial = 1;
+    cout << "Enter a positive integer: ";
+    cin >> n;
+    if (n < 0) {
+        cout << "Factorial is not defined for negative numbers." << endl;
+    } else {
+        for (int i = 1; i <= n; ++i) {
+            factorial *= i; // Multiply factorial by i in each iteration
+        }
+        cout << "Factorial of " << n << " = " << factorial << endl;
+    }
+    return 0;
+}
