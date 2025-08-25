619e8ac91644ea10d54e9d9b46a9ffa8520e8f34
shariarhossaintanvir <bangboombro629@gmail.com>
2025-08-26 02:19:17 +0600

    Add Initial Files

---
+++ 111_int_to_chr.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+int num;
+char ch;
+cout << "Enter an integer (0-9): ";
+cin >> num;
+ch = num + '0';
+cout << "The character is: " << ch << endl;
+return 0;
+}
