a05aad3f7d9567f85b5bcb3232e61c8c55f7da57
shariarhossaintanvir <bangboombro629@gmail.com>
2025-08-27 01:19:22 +0600

    Add Initail Files

---
+++ 112_char_to_int.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+char ch;
+cout << "Enter a character: ";
+cin >> ch;
+int asciiValue = ch;  // Char to Int (ASCII)
+cout << "Integer value: " << asciiValue << endl;
+return 0;
+}
