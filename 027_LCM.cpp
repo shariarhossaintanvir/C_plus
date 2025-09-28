d7ed4e1c74ee7347b926c330b55a272769d6bf0b
shariarhossaintanvir <bangboombro629@gmail.com>
2025-09-28 16:44:21 +0600

    Add Initial Files

---
+++ 027_LCM.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num1, num2, lcm;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    lcm = (num1 > num2) ? num1 : num2;
+
+while (true) {
+if (lcm % num1 == 0 && lcm % num2 == 0) {
+cout << "LCM of " << num1 << " and " << num2 << " is " << lcm << endl;
+break;
+}
+lcm++;
+}
+return 0;
+}
