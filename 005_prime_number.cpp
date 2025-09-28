47780ba4cb3db7574eaec91392fbcafbf426a487
shariarhossaintanvir <bangboombro629@gmail.com>
2025-09-28 13:51:36 +0600

    Add Input In Program

--- 005_prime_number.cpp
+++ 005_prime_number.cpp
@@ -2,9 +2,13 @@
 using namespace std;
 int main() {
 int num, i;
+cout<<"Enter a Number : ";
 cin >> num;
 for (i = 2; i < num; i++)
-if (num % i == 0) break;
-if (num > 1 && i == num) cout << "Prime";
- else cout << "Not Prime";
+if (num % i == 0) 
+break;
+if (num > 1 && i == num)
+cout << "Prime";
+ else
+ cout << "Not Prime";
 }
