08ba897b8f8aa3973cc558dc10f70e9a77786d00
shariarhossaintanvir <bangboombro629@gmail.com>
2025-09-28 13:59:20 +0600

    Add Initial Files

--- 003_print_integer.cpp
+++ 003_print_integer.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main() {
 int number;
 cout << "Enter an integer: ";
- cin >> number;
+cin >> number;
 cout << "You entered: " << number << endl;
 return 0;
 }
--- 007_ascii_value.cpp
+++ 007_ascii_value.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main() {
 char ch;
- cout << "Enter a character: ";
- cin >> ch;
+cout << "Enter a character: ";
+cin >> ch;
 cout << "The ASCII value of " << ch << " is " << int(ch) << endl;
 return 0;
 }
